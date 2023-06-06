0ce18fb914ccbab400730cade48e6392461a89c4
Jun2-Lee <ljh19980823@gmail.com>
2023-06-06 12:01:15 +0900

    fix : RSS Map 수정

--- whereami/src/main/java/com/junsim/whereami/controller/ApController.java
+++ whereami/src/main/java/com/junsim/whereami/controller/ApController.java
@@ -1,15 +1,19 @@
 package com.junsim.whereami.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import service.ApService;
+
+import java.util.HashMap;
 
 @RestController
+@RequiredArgsConstructor
 public class ApController {
-
-    @PostMapping("/test")
-    public void Test(@RequestBody String rss){
-        System.out.println("rss = " + rss);
+    private final ApService apService;
+    @PostMapping("/ap")
+    public void fingerPrint(@RequestBody HashMap<String ,Integer> rss){
+        apService.fingerprint(rss);
     }
 }
