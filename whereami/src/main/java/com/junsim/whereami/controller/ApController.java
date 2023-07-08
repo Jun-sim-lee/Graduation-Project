f29b3d327b47950787d71c08bec739fb6096798f
Jun2-Lee <ljh19980823@gmail.com>
2023-07-08 20:47:16 +0900

    feat : whereami 초기 프로젝트 셋팅, 지도 완성

--- whereami/src/main/java/com/junsim/whereami/controller/ApController.java
+++ whereami/src/main/java/com/junsim/whereami/controller/ApController.java
@@ -1,10 +1,12 @@
 package com.junsim.whereami.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import service.ApService;
+import com.junsim.whereami.service.ApService;
 
 import java.util.HashMap;
 
@@ -12,8 +14,10 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 public class ApController {
     private final ApService apService;
+
     @PostMapping("/ap")
     public void fingerPrint(@RequestBody HashMap<String ,Integer> rss){
         apService.fingerprint(rss);
+
     }
 }
