93fd47e3f1a12dd9d891d783c612fdb891ec083b
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-08-02 00:52:14 +0900

    feat: vuex의 state 및 actions 동작 확인

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,8 +1,8 @@
 <template>
     <div class = "screen">
         <img src="../assets/logo.png" class = "logo">
-        <input class = "wmi_input" style = "top: 422px; padding-left: 3%;" placeholder="아이디" v-model="userInfoDto.userId"/>
-        <input type = "password" class = "wmi_input" style = "top: 477px; padding-left: 3%;" placeholder="비밀번호" v-model="userInfoDto.password"/>
+        <input class = "wmi_input" style = "top: 422px; padding-left: 3%;" placeholder="아이디" v-model="loginDto.userId"/>
+        <input type = "password" class = "wmi_input" style = "top: 477px; padding-left: 3%;" placeholder="비밀번호" v-model="loginDto.password"/>
         <button class = "submit_form" style = "top: 570px" @click="sendRequest">로그인</button>
     </div>
 </template>
@@ -11,22 +11,29 @@
 import { router } from '@/router';
 import axios from 'axios'
 import {ref} from 'vue'
+import { useStore } from 'vuex';
 
-const userInfoDto = ref({
+const store = useStore();
+const loginDto = ref({
         userId: "",
         password: ""
 })
 
 function sendRequest(){
-    axios.post("http://localhost:8080/login", userInfoDto.value)
+    axios.post("http://localhost:8080/login", loginDto.value)
          .then((resp) => {
-            if(resp.data.accessToken)
-                localStorage.setItem('user', JSON.stringify(resp.data));
-            if(resp.data === "user")
+            if(resp.status === 200){
+                store.commit("login", resp)
+                localStorage.setItem('user', JSON.stringify(resp.data.accessToken));
+            }
+            
+            console.log(store.state.accessToken)
+            
+            if(resp.data.authority === "user")
                 router.replace('main')
-            else if(resp.data === "student")
+            else if(resp.data.authority === "student")
                 router.replace('otp')
-            else if(resp.data === "professor")
+            else if(resp.data.authority === "professor")
                 router.replace('qr')
             else
                 alert("없는 유저 입니다.")
