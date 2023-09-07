3263a573fe96f8049effd00f3926f830465fc401
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-09-08 00:03:18 +0900

    feat: 리소스 목록 및 인증 기능 전체 백엔드와 병합 완료
    - 리소스 목록 출력 기능 병합 완료
    - 인증 기능 전체 병합 완료
      - 이메일 인증
      - OTP 인증
      - QR 인증
      - 기기 인증
        - 기기 인증 부분에서 라즈베리파이 아이콘 움직이는 효과 추가

---
+++ src/views/MachineAuthView.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class = "screen">
+        <img src="../assets/logo.png" class = "logo"/>
+        <img :src="rpiImage" style="position: absolute; top: 310px;"/>
+        <h1 style="position: absolute; top: 420px; font-size: 20px">기기
+            <span style="color: gray;">(라즈베리파이)</span>
+            를 켜주시기 바랍니다.</h1>
+        <button class="submit_form" @click="sendRequest" style="top: 500px">완료</button>
+    </div>
+</template>
+
+<script setup>
+import { router } from '@/router'
+import axios from 'axios';
+import {ref, inject, onMounted, onUnmounted} from 'vue'
+import { useStore } from 'vuex';
+
+const store = useStore();
+const headers = JSON.parse(inject('headers') + store.state.accessToken + '"}');
+const requestURL = inject('requestURL')
+const rpiImage = ref(require("../assets/rpiauth1.png"))
+const imageCount = ref(0)
+var timer;
+
+onMounted(() => {
+    timer = setInterval(changeImage, 1000);
+})
+
+onUnmounted(() => {
+    clearInterval(timer);
+})
+
+function sendRequest(){
+    axios.get(requestURL + 'checkRpiOn', {headers})
+     .then((resp) => {
+        if(resp.data === true){
+            alert("라즈베리파이가 확인되었습니다.\n입장합니다.")
+            if(store.state.authority === "None")
+                router.replace('emailAuth')
+
+            router.replace('main')
+        }
+     })
+     .catch((error) => {
+        if(error.response.data.error.message){
+            alert("유효하지 않은 고유 코드입니다!!\n메인화면으로 이동합니다.")
+            localStorage.clear();
+            router.replace('')
+        }
+     })
+}
+
+function changeImage(){
+    imageCount.value++
+    rpiImage.value = require("../assets/rpiauth" + ((imageCount.value)%4 + 1) + ".png")   
+}
+</script>
+
+<style>
+@import "../style/wmistyle.css";
+</style>
